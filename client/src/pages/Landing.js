70816bae21ff9bcccbc01d41530a64375798398e
Foresight-Tech <foresightagencies@gmail.com>
2022-08-17 21:46:53 -0700

    first commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            We know getting your dream job may sometimes feel exhausting and impossible to keep track of, so offers you real time job tracking so you can monitor your applications till you land that dream job.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
